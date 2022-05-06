07adba4db9b7b0ca9c548475e601ab1ec3485392
Anthony Graca <anthonygraca@gmail.com>
2022-05-06 15:09:12 -0700

    added complex expressions for ex1

--- test/sicp/ch1/ex1_test.clj
+++ test/sicp/ch1/ex1_test.clj
@@ -35,3 +35,12 @@
   (is (= 16 (cond (= a 4) 6
                   (= b 4) (+ 6 7 a)
                   :else 25))))
+
+(deftest test-compound-operation-with-predicate
+  (is (= 6 (+ 2 (if (> b a) b a)))))
+
+(deftest test-compound-operation-with-conditional
+  (is (= 16 (* (cond (> a b) a
+                     (< a b) b
+                     :else -1)
+               (+ a 1)))))
