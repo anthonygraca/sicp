986e373bf57c3af2b45fd43ba5d6277a6e24b63c
anthonygraca <anthonygraca@gmail.com>
2022-05-21 11:40:21 -0700

    complete exercise 1.1

--- src/sicp/ch1/exercises.clj
+++ src/sicp/ch1/exercises.clj
@@ -1,1 +1,6 @@
 (ns sicp.ch1.exercises)
+
+;; Exercise 1.1
+(def a 3)
+
+(def b (+ a 1))
--- test/sicp/ch1/exercises_test.clj
+++ test/sicp/ch1/exercises_test.clj
@@ -1,3 +1,54 @@
 (ns sicp.ch1.exercises-test
   (:require [sicp.ch1.exercises :refer :all]
             [clojure.test :refer :all]))
+
+;; Exercise 1.1
+(deftest test-expression-result-1
+  (is (= 10 10)))
+
+(deftest test-expression-result-2
+  (is (= 12 (+ 5 3 4))))
+
+(deftest test-expression-result-3
+  (is (= 8 (- 9 1))))
+
+(deftest test-expression-result-4
+  (is (= 3 (/ 6 2))))
+
+(deftest test-expression-result-5
+  (is (= 6 (+ (* 2 4) (- 4 6)))))
+
+(deftest test-define-a
+  (is (= 3 a)))
+
+(deftest test-define-b
+  (is (= 4 b)))
+
+(deftest test-expression-result-6
+  (is (= 19 (+ a b (* a b)))))
+
+(deftest test-bool-expression
+  (is (= false (= a b))))
+
+(deftest test-if-statement
+  (is (= b
+         (if (and (> b a) (< b (* a b)))
+           b
+           a))))
+
+(deftest test-cond-statement
+  (is (= 16
+         (cond (= a 4) 6
+               (= b 4) (+ 6 7 a)
+               :else 25))))
+
+(deftest test-if-expression
+  (is (= 6
+         (+ 2 (if (> b a) b a)))))
+
+(deftest test-cond-expression
+  (is (= 16
+         (* (cond (> a b) a
+                  (< a b) b
+                  :else -1)
+            (+ a 1)))))
